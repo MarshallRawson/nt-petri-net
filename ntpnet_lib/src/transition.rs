bcbdc35933f62b3ba2fb3688c0f38cdd82dd1eaf
MarshallRawson <marshallrawson@ufl.edu>
2022-09-17 18:41:42 -0500

    almost can execute

--- ntpnet_lib/src/transition.rs
+++ ntpnet_lib/src/transition.rs
@@ -3,11 +3,13 @@ use std::any::TypeId;
 
 use crate::Token;
 
+#[derive(Debug)]
 pub struct Description {
     pub in_edges: HashSet<(String, TypeId)>,
     pub out_edges: HashSet<(String, TypeId)>,
     pub cases: HashMap<String, Case>,
 }
+#[derive(Debug)]
 pub struct Case {
     pub conditions: Vec<HashSet<(String, TypeId)>>,
     pub products: Vec<HashSet<(String, TypeId)>>,
@@ -20,3 +22,11 @@ pub trait Transition {
             out_map: &mut HashMap<(String, TypeId), Token>,
     ) -> usize;
 }
+
+use std::fmt::{Debug, Result};
+impl Debug for dyn Transition {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result {
+        write!(f, "Transition{{{:#?}}}", self.description())
+    }
+}
+
