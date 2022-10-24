4575bc81fe8e45a62e80f4400ba214514b89a849
MarshallRawson <marshallrawson@ufl.edu>
2022-10-24 13:44:49 -0500

    camera debug config

--- ntpnets/src/camera_reader.rs
+++ ntpnets/src/camera_reader.rs
@@ -20,14 +20,23 @@ pub struct CameraReader {
     p: PlotSink,
 }
 impl CameraReader {
-    pub fn maker(fps: u32, plotsink: PlotSink) -> TransitionMaker {
+    pub fn maker(fps: u32, mut plotsink: PlotSink) -> TransitionMaker {
         Box::new(move || {
             let mut cam = Camera::new("/dev/video0").unwrap();
-            cam.start(&Config {
+            let config = Config {
                 interval: (1, fps),
                 format: b"RGB3",
                 ..Default::default()
-            }).unwrap();
+            };
+            plotsink.println2("debug",&format!(
+                "frame rate: {} / {} Hz", config.interval.1, config.interval.0
+            ));
+            plotsink.println2("debug",&format!("resolution: {} x {} Hz",
+                 config.resolution.0, config.resolution.1
+            ));
+            plotsink.println2("debug",&format!("format: {}", std::str::from_utf8(config.format).unwrap()));
+            plotsink.println2("debug",&format!("nbuffers: {}", config.nbuffers));
+            cam.start(&config).unwrap();
             Box::new(Self {
                 camera: cam,
                 start_time: Instant::now(),
@@ -39,7 +48,7 @@ impl CameraReader {
     fn read(&mut self, _: Input) -> Output {
         let frame = self.camera.capture().unwrap();
         let rgb_frame =
-            RgbImage::from_raw(frame.resolution.0, frame.resolution.1, (&frame[..]).to_vec()).unwrap();
+            RgbImage::from_raw(frame.resolution.0, frame.resolution.1, frame.to_vec()).unwrap();
         let now = Instant::now();
         if let Some(last_time) = self.last_time {
             self.p.plot_series_2d(
